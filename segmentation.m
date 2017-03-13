1af2a4ac8763e251c60bf4494065fade3c89c82d
Yukyung Choi <unizard@gmail.com>
2017-03-13 09:55:14 +0900

    Create segmentation.m

---
+++ segmentation.m
@@ -0,0 +1,2 @@
+
+Hi all, I've been trying to figure out how segmentation CNNs work. I have a good understanding of how CNNs work in labelling an input image, but am confused how segmentation networks are able to assign a label to each pixel? How is the data passed into the network? Is it based on some sliding kernel where sections of the image are passed in? Any good papers I can read to figure this out?
